f4fd7eecd0e772f3fccaffbfed95faca114c4491
subashkhadka <subash.khadka@epfnepal.com.np>
2020-10-15 16:49:16 +0545

    new commit

--- mypython.py
+++ mypython.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = 'CICD from python!\n'
+    output = b'CICD from python!\n'
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
